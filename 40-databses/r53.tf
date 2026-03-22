4f63aac043b1103b70ca6047fdb4c695adec08cc
msrtgit <msrt10.com@gmail.com>
2026-03-22 18:10:05 +0530

    repo

--- 40-databses/r53.tf
+++ 40-databses/r53.tf
@@ -13,3 +13,19 @@
   ttl     = 1
   records = [aws_instance.redis.private_ip]
 }
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+}
