c846614e5810ccd897de539a743290fd7190569e
msrtgit <msrt10.com@gmail.com>
2026-03-23 17:44:26 +0530

    pushed

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "git::https://github.com/PrudhviMunakala/terraform-vpc-module.git?ref=main"
-    source = "terraform-vpc-module"
+    source = "../../terraform-vpc-module"
     project = var.project
     environment = var.environment
     enable_peering = false
